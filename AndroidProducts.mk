d7c7daca1a393235eabb2d70295a3f9b9c772f13
official-mocha <officialmocha111@gmail.com@gmail.com>
2022-02-01 19:49:18 -1100

    tulip: Bringup Pixel Extended

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/komodo_tulip.mk
+    $(LOCAL_DIR)/aosp_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    komodo_tulip-user \
-    komodo_tulip-userdebug \
-    komodo_tulip-eng
+    aosp_tulip-user \
+    aosp_tulip-userdebug \
+    aosp_tulip-eng
