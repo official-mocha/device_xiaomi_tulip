f861ac05f260fd9b4173182c76eb68015cb5afe6
official-mocha <officialmocha111@gmail.com>
2022-07-25 18:03:51 +0530

    tulip: bringup awakenos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/komodo_tulip.mk
+    $(LOCAL_DIR)/awaken_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    komodo_tulip-user \
-    komodo_tulip-userdebug \
-    komodo_tulip-eng
+    awaken_tulip-user \
+    awaken_tulip-userdebug \
+    awaken_tulip-eng
